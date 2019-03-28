05712865dae1883e7607495621f046f330f54522
zhaofei <13700530350@163.com>
2019-03-28 23:35:29 +0800

    聊天

--- pages/message/message.js
+++ pages/message/message.js
@@ -2,19 +2,13 @@
 var conf = require('../../resource/js/conf.js'),
     util = require('../../utils/util.js');
 
-// 
-var socketOpen = false;
-var frameBuffer_Data, session, SocketTask;
-var url = 'ws://请填写您的长链接接口地址';
-var upload_url ='请填写您的图片上传接口地址'
-
 //获取应用实例
 var app = getApp();
 Page({
 	data: {
-
+		'userId':''
 	},
-	onLoad: function () {
-		
+	onLoad: function (e) {
+		console.log(e);
 	}
 })
--- pages/userlist/userlist.js
+++ pages/userlist/userlist.js
@@ -14,42 +14,49 @@ Page({
   // 首页展示数据
   onLoad: function () {
     var self = this;
-    // wx.checkSession({
-    //     success: function(e){
-    //         console.log("没过期");
-    //         wx.getUserInfo({
-    //           success(res) {
-    //              self.getUserData(res);
-    //           }
-    //         })
-    //     },
-    //     fail: function(){ 
-    //         console.log("过期了");
-    //         wx.login({
-    //             success: function(res) {
-    //                 if (res.code) {
-    //                   wx.getUserInfo({
-    //                     success(res) {
-    //                       self.getUserData(res);
-    //                     }
-    //                   })
-    //                 } else {
-    //                     console.log('获取用户登录态失败！' + res.errMsg)
-    //                 }
-    //             }
-    //         });
-    //     }
-    // });
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    }else{
+      wx.login({
+          success: function(res) {
+              if (res.code) {
+                wx.getUserInfo({
+                  success(e) {
+                    self.getUserData(e,res);
+                  }
+                })
+              } else {
+                  console.log('获取用户登录态失败！' + res.errMsg)
+                  wx.showToast({
+                    title: '获取用户登录态失败！' + res.errMsg,
+                    icon: 'none',
+                    duration: 2000
+                  });
+              }
+          }
+      });
+    }
   },
   maskmove:function(){},
   getUserInfo: function(e) {
     var self = this;
-    console.log(e)
     if (e.detail.userInfo) {
       // 发送 res.code 到后台换取 openId, sessionKey, unionId
       wx.login({
         success: res => {
-            self.getUserData(res);
+            self.getUserData(e.detail,res);
         }
       })  
     } else {
@@ -61,22 +68,29 @@ Page({
       });
     }
   },
-  getUserData:function(e){
-    console.log(e);
-    return '';
+  getUserData:function(userinfo,code){
     var self = this;
-    app.globalData.userInfo = e.userInfo;
-    this.setData({
-      userInfo: e.userInfo,
-      hasUserInfo: true
-    })
+    
     util.request({
         url:conf.getUserInfoUrl,
-        data:{'userInfo':e.userInfo,'encryptedData':e.encryptedData,'iv':e.iv},
+        data:{'userInfo':userinfo.userInfo,'encryptedData':userinfo.encryptedData,'iv':encodeURIComponent(userinfo.iv),'code':code.code},
         method:'POST',
         header:{'content-type': 'application/x-www-form-urlencoded'},
         success: function (callback) {
-          console.log(callback);          
+          console.log(callback);
+          if(!callback.data.flag){
+              wx.showToast({
+                title: callback.data.msg,
+                icon: 'none',
+                duration: 2000
+              });
+          }else{
+              app.globalData.userInfo = callback.data.data;
+              self.setData({
+                userInfo: callback.data.data,
+                hasUserInfo: true
+              })
+          }
         }
     })
   }
